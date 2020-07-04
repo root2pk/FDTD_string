38f888516b9194f81778b463f8deb075e5bfd22c
Ruthu Prem Kumar <60438163+root2pk@users.noreply.github.com>
2020-07-04 13:21:49 +0100

    Add files via upload

---
+++ A4_Chord_Program_s1760559_Premkumar.m
@@ -0,0 +1,67 @@
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% % %     CHORD GENERATOR WITH CUSTOMISATION
+% % %     RUTHU PREM KUMAR 
+% % %     MARCH 2020
+% % %     
+% % %     
+% % %     THIS PROGRAM CALLS A FUNCTION WHICH USES THE SINGLE STRING FDTD
+% % %     FUNCTION TO GENERATE CHORDS
+% % %     THE PARAMETERS THAT CAN BE CHANGED ARE
+% % %     
+% % %     THE CHORD ITSELF
+% % %     THE GAUGE OF THE STEEL STRINGS
+% % %     THE METHOD OF STRIKING (DOWN OR UP)
+% % %     THE GAP BETWEEN CONSECUTIVE STRIKES
+% % %     
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+clear all; close all;
+
+%% String length when fret is held (in metres)
+
+f0 = 0.6477;       % open
+f1 = 0.6113;       % 1st fret
+f2 = 0.5771;       % 2nd fret      
+f3 = 0.5447;       % 3rd fret
+f4 = 0.5142;       % 4th fret
+f5 = 0.4854;       % 5th fret
+
+%% Frequencies (In Hz)
+% This forms a 2X6 matrix with the first row providing frequency data and
+% the second row providing string length data
+
+Am_chord = [82.41,110,164.81,220,261.63,329.63;f0,f0,f2,f2,f1,f0];
+C_chord = [82.41,130.81,164.81,196,261.63,329.63;f0,f3,f2,f0,f1,f0];
+G_chord = [98,123.47,146.83,196,293.66,392;f3,f2,f0,f0,f3,f3];
+Em_chord = [82.41,123.47,164.81,196,246.94,329.63;f0,f2,f2,f0,f0,f0];
+F_chord = [87.31,130.81,174.61,220,261.63,349.23;f1,f3,f3,f2,f1,f1];
+Dm_chord = [87.31,110,146.83,220,293.66,349.23;f1,f0,f0,f2,f3,f1];
+D_chord = [92.50,110,146.83,220,293.66,369.99;f2,f0,f0,f2,f3,f2];
+Bm_chord = [92.50,123.47,185,246.94,293.66,369.99;f2,f2,f4,f4,f3,f2];
+Bflat_chord = [87.31,116.54,174.61,233.08,293.66,349.23;f1,f1,f3,f3,f3,f1];
+Gm_chord = [98,146.83,196,233.08,293.66,392;f3,f5,f5,f3,f3,f3];
+
+%% String Gauges (in inches)
+% From lightest to heaviest
+
+Extra_Light = [0.010,0.014,0.023,0.030,0.039,0.047];
+Custom_Light = [0.011,0.015,0.023,0.032,0.042,0.052];
+Light = [0.012,0.016,0.025,0.032,0.042,0.054]; 
+Medium = [0.013,0.017,0.026,0.035,0.045,0.056]; 
+Heavy = [0.014,0.018,0.027,0.039,0.049,0.059]; 
+
+% From heaviest to lightest
+Extra_Light = flip(Extra_Light);
+Custom_Light = flip(Custom_Light);
+Light = flip(Light);
+Medium = flip(Medium);
+Heavy = flip(Heavy);
+%%%%%%%%%%%%%%%% -------------------%%%%%%%%%%%%%%%%%%%%
+
+%% Call chord function
+% Format - A4_Chord_s1760559_Premkumar(<chord name>,<gauge name>,'up'/'down',strike gap(s))
+% If neither up nor down is specified, the function automatically plays all
+% the strings together as a block chord
+A4_Chord_s1760559_Premkumar(Am_chord,Heavy,'down',0.1);
+
---
+++ A4_Six_String_FDTD_s1760559_Premkumar.m
@@ -0,0 +1,110 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% PBMMI SIX STRING FDTD ASSIGNMENT 
+%%%%% 
+%%%%%
+%%%%%
+%%%%% PROGRAM THAT CALLS THE FDTD SINGLE STRING FUNCTION FOR SIX DIFFERENT STRINGS
+%%%%% OF VARIOUS PARAMETERS.
+%%%%% THE STRINGS ARE OF A STEEL STRING GUITAR TUNED EADGBE.
+%%%%%
+%%%%% THE OUTPUT IS A STEREO OUTPUT WITH EACH STRING AT DIFFERENT PAN
+%%%%% LEVELS
+%%%%%
+%%%%% References : 
+%%%%% https://pages.mtu.edu/~suits/notefreqs.html
+%%%%% https://www.dawsons.co.uk/blog/acoustic-guitar-strings-guide
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all
+
+%% Options
+opts.plot_on = false;
+opts.useforloop = true;
+opts.add_stiffness = false;
+opts.input_type = 'plucked';
+opts.output_type = 'displacement';
+%opts.bctype = 'clamped';
+opts.bctype = 'simply_supported';
+
+%% Fixed Physical Parameters 
+
+phys_param.E = 2e11;                   % Young's modulus (Pa)
+phys_param.L = 0.6477;                 % length (m)
+phys_param.T60 = 5;                    % T60 (s)
+phys_param.rho = 7850;                 % density (kg/m^3)
+exc_interval = 0.5;                    % Excitation interval (s)
+
+%% Arrays for multiple strings
+
+% List of string frequencies (E2,A2,D3,G3,B3,E4)
+freq_list = [82.41,110,146.83,196,246.94,329.63];
+
+% List of string radii (Edit based on gauge values in inches)
+r_list = [0.054,0.042,0.032,0.025,0.016,0.012];
+
+% Converting inches to metres
+r_list = r_list.*0.00254;
+
+% Tension list obtained using the formula ( f = (1/2L)*sqrt(T/M) )
+T_list = 4*pi*phys_param.rho*(phys_param.L^2)*(freq_list.^2).*(r_list.^2);
+
+% Excitation time list
+exc_list = [0:exc_interval:5*exc_interval];
+
+% Excitation coordinate list
+xi_list = [0.7,0.8,0.85,0.75,0.8,0.77];
+
+% Peak Amplitue list
+famp_list = [5,5,5,5,5,5];
+
+% Duration of Excitation list
+dur_list = [0.001,0.002,0.001,0.003,0.002,0.001];
+
+% Coordinate of Output List
+xo_list = [0.1,0.1,0.1,0.1,0.1,0.1];
+
+% Stereo list
+stereo_list = linspace(-1,1,6);
+
+%% Fixed Simulation parameters 
+sim_param.SR = 44100;                  % sample rate (Hz)
+sim_param.Tf = 4;                      % duration of simulation (s)
+
+
+%% Output Signal
+% Output Signal Length
+
+output_L = zeros((5*exc_interval + sim_param.Tf)*sim_param.SR+1,1);  % Length of left output signal in samples
+output_R = output_L;                                                 % Length of right output signal in samples
+exc_interval = exc_interval*sim_param.SR;                            % Excitation interval in samples
+for i = 1:6
+    %% Variable physical parameters 
+    phys_param.T = T_list(i);          % tension (N)
+    phys_param.r = r_list(i);          % string radius (m)
+    sim_param.exc_st = exc_list(i);    % start time of excitation (s)
+    sim_param.xi = xi_list(i);         % coordinate of excitation (normalised, 0-1)
+    sim_param.famp = famp_list(i);     % peak amplitude of excitation (N)
+    sim_param.dur = dur_list(i);       % duration of excitation (s)
+    sim_param.xo = xo_list(i);         % coordinate of output (normalised, 0-1)
+    y = A4_String_FDTD_s1760559_Premkumar(opts,phys_param,sim_param); 
+    
+    %% Stereo properties
+    
+    % Contribution to each channel based on pan level
+    coeff1 = abs(1 - stereo_list(i));  
+    coeff2 = abs(-1 - stereo_list(i));    
+    y_L = coeff1*y;
+    y_R = coeff2*y;  
+    
+    % Adding all the signals into two channels
+    start_pos = exc_interval*(i-1)+1;
+    end_pos = start_pos + sim_param.Tf*sim_param.SR - 1;
+    output_L(start_pos:end_pos) = output_L(start_pos:end_pos) + y_L;
+    output_R(start_pos:end_pos) = output_R(start_pos:end_pos) + y_R;
+    
+end
+
+% Converting two channels to one stereo signal
+output = [output_L,output_R];
+
+%% Play sound
+soundsc(output,sim_param.SR);
